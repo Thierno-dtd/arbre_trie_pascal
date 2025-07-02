ffc075414d7e2573f8144ccfb1c573ac469aad1c
Thierno-dtd <davidouthe2@gmail.com>
2025-07-02 04:00:53 +0100

    ...

--- mainArbre.pas
+++ mainArbre.pas
@@ -3,32 +3,53 @@ program mainArbre;
 uses listePointeur, fonctionElementaireArbre, arbreTrieApplicatif;
 
 var
-    L, Listtriee: liste;
+    L, ListTriee: liste;
     a: arbre;
     i, n, val: T;
 
 begin
+    writeln('🌳 Bienvenue dans le programme de tri par Arbre Binaire Trié 🌳');
+    writeln('-------------------------------------------------------------');
+
     { Création de la liste initiale }
     vide(L);
-    writeln('Combien d''éléments dans la liste ? ');
+    writeln;
+    writeln('📝 Combien de valeurs souhaitez-vous entrer dans la liste ?');
+    write('👉 Votre choix : ');
     readln(n);
 
-    writeln('Entrez les ', n, ' valeurs : ');
+    writeln;
+    writeln('👉 Super ! Entrez maintenant les ', n, ' valeurs (une par ligne) :');
     for i := 1 to n do
     begin
+        write('🔢 Valeur ', i, ' : ');
         readln(val);
         inserer(L, i, val);
     end;
 
-    { Construction de l''arbre binaire trié à partir de la liste }
-    construireArbre(L, a);
+    writeln;
+    writeln('✅ Liste initiale enregistrée avec succès !');
+    writeln('-------------------------------------------');
 
-    { Parcours infixé pour obtenir la liste triée }
-    infix(a, Listtriee);
+    { Construction de l'arbre }
+    construireArbre(L, a);
+    writeln('🌱 Arbre binaire trié construit avec succès !');
+
+    { Parcours infixé pour trier }
+    writeln('🔎 Génération de la liste triée (parcours infixé en cours)...');
+    infix(a, ListTriee);
 
     { Affichage de la liste triée }
-    writeln('Liste triée :');
-    for i := 1 to longueur(Listtriee) do
-        writeln(ieme(Listtriee, i));
+    writeln;
+    writeln('✅ Résultat final : Liste triée par ordre croissant :');
+    writeln('-----------------------------------------------------');
+    for i := 1 to longueur(ListTriee) do
+    begin
+        write('➡️  ');
+        writeln(ieme(ListTriee, i));
+    end;
 
+    writeln;
+    writeln('🎉 Fin du programme. Merci d''avoir utilisé notre arbre trié !');
+    writeln('---------------------------------------------------------------');
 end.
